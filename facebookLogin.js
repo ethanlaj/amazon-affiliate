fb94dee8a8dd206b6eebe6f251dd95034814add0
ethanlaj <ethan@lajfam.com>
2021-06-14 19:23:15 -0400

    pie

--- facebookLogin.js
+++ facebookLogin.js
@@ -3,24 +3,24 @@ const ms = require('ms');
 
 module.exports.run = async function (browser, first=false) {
 	const page = await browser.newPage();
-        if (first) {
-        await page.goto('https://www.facebook.com/login');
-	try {
-		await page.type('#email', process.env.EMAIL);
-		await page.type('#pass', process.env.FB_PW);
+	if (first) {
+		await page.goto('https://www.facebook.com/login');
+		try {
+			await page.type('#email', process.env.EMAIL);
+			await page.type('#pass', process.env.FB_PW);
 
-		await Promise.all([
-			page.waitForNavigation(),
-			page.click('#loginbutton')
-		]);
+			await Promise.all([
+				page.waitForNavigation(),
+				page.click('#loginbutton')
+			]);
 
-		console.log('Logging into Facebook...');
-	} catch {
-		console.log('Most likely already logged into Facebook, skipping logging in again.');
+			console.log('Logging into Facebook...');
+		} catch {
+			console.log('Most likely already logged into Facebook, skipping logging in again.');
+		}
+
+		await wait(ms('1m'));
 	}
-
-       await wait(ms('1m'));
-}
 
 	await page.goto('https://www.facebook.com/groups/amazeballdeals');
 
--- newBrowser.js
+++ newBrowser.js
@@ -3,11 +3,12 @@ const puppeteer = require('puppeteer');
 module.exports.run = async function () {
 	let options = {
 		headless: true,
-		args: ['--no-sandbox']
 	};
-	if (process.env.ENVIRONMENT === 'local') {
+	if (process.env.ENVIRONMENT === 'local')
 		options.executablePath = './node_modules/puppeteer/.local-chromium/mac-884014/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
-	}
+	if (process.env.ENVIRONMENT === 'pi')
+		options.args = ['--no-sandbox', '--disable-setuid-sandbox'];
+
 	let browser = await puppeteer.launch(options);
 
 	let context = browser.defaultBrowserContext();
