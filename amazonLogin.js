e7cd9dc5f24154b5e485d6cca014bb4b95335b8e
ethanlaj <ethan@lajfam.com>
2021-06-14 21:03:26 -0400

    hm

--- amazonLogin.js
+++ amazonLogin.js
@@ -1,6 +1,3 @@
-const wait = require('./wait').run;
-const ms = require('ms');
-
 module.exports.run = async function (browser) {
 	const page = await browser.newPage();
 	await page.goto('https://affiliate-program.amazon.com/home/promohub/promocodes');
@@ -18,8 +15,6 @@ module.exports.run = async function (browser) {
 	} catch {
 		console.log('Most likely already logged into Amazon, skipping logging in again.');
 	}
-
-	await wait(ms('1m'));
 
 	return page;
 };
