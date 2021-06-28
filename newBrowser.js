d99f3d8cc58cfe9a6a2ad4df953711e0bc4e773b
ethanlaj <ethan@lajfam.com>
2021-06-28 18:53:05 -0400

    t

--- newBrowser.js
+++ newBrowser.js
@@ -13,6 +13,8 @@ module.exports.run = async function () {
 
 	let browser = await puppeteer.launch(options);
 
+	console.log(browser.userAgent);
+
 	let context = browser.defaultBrowserContext();
 	context.overridePermissions('https://www.facebook.com', ['geolocation', 'notifications']);
 
