f3f9f2704c4d79eb6cb78b423a5b432b45c30f5b
ethanlaj <ethan@lajfam.com>
2021-06-29 00:01:37 -0400

    a

--- newBrowser.js
+++ newBrowser.js
@@ -7,13 +7,11 @@ module.exports.run = async function () {
 	if (process.env.ENVIRONMENT === 'local')
 		options.executablePath = './node_modules/puppeteer/.local-chromium/mac-884014/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
 	if (process.env.ENVIRONMENT === 'pi') {
-		options.args = ['--no-sandbox'];
+		options.args = ['--no-sandbox', '--disable-setuid-sandbox'];
 		options.executablePath = '/usr/bin/chromium-browser';
 	}
 
 	let browser = await puppeteer.launch(options);
-
-	console.log(await browser.version());
 
 	let context = browser.defaultBrowserContext();
 	context.overridePermissions('https://www.facebook.com', ['geolocation', 'notifications']);
