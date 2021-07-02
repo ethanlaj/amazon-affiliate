e1b735930ca5bf1ec86664c9ea2d1f05b7509a3e
ethanlaj <ethan@lajfam.com>
2021-07-02 02:16:20 -0400

    disable 1 acc, increase times between running each acc

--- passwords.js
+++ passwords.js
@@ -2,8 +2,8 @@ module.exports = {
 	amazon: { email: 'ethan@lajfam.com', pw: 'elaj@4203' },
 	facebook: [
 		{ id: 1, email: 'ethan@lajfam.com', pw: 'Elajeunesse@4203'},
-		/*{ id: 2, email: 'ethanlaj.business@gmail.com', pw: 'elaj@4203'},
+		{ id: 2, email: 'ethanlaj.business@gmail.com', pw: 'elaj@4203'},
 		{ id: 3, email: 'ethanlaj.code@gmail.com', pw: 'elaj@4203'},
-		{ id: 4, email: 'amazeballdeals@gmail.com', pw: 'elaj@4203'}*/
+		//{ id: 4, email: 'amazeballdeals@gmail.com', pw: 'elaj@4203'}
 	]
 };
